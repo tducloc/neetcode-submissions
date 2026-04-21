bbfda7e78eff0ee2f2318576dcdde9a0b8abc335
Trần Đức Lộc <ducloc0009@gmail.com>
2026-04-21 22:13:28 +0700

    Add: daily-temperatures - submission-2

---
+++ Data Structures & Algorithms/daily-temperatures/submission-2.js
@@ -0,0 +1,49 @@
+class Solution {
+    /**
+     * @param {number[]} temperatures
+     * @return {number[]}
+     */
+    dailyTemperatures(temperatures) {
+        // Bruteforce
+        // Tai ngay thu i tim ngay sau ma nhiet do > temp[i] => result = higher_temp[i].index - i
+
+        // const result = Array(temperatures.length).fill(0);
+
+        // for (let i = 0; i < temperatures.length; i++) {
+        //     const temp = temperatures[i];
+        //     for (let j = i + 1; j < temperatures.length; j++) {
+        //         if (temperatures[j] > temp) {
+        //             result[i] = j - i;
+        //             break;
+        //         }
+        //     }
+        // }
+
+        // return result;
+
+        // Main
+        const result = Array(temperatures.length).fill(0);
+        const stack = [];
+
+        for (let i = 0; i <= temperatures.length; i++) {
+            if (stack.length === 0) {
+                stack.push(i);
+                continue;
+            }
+
+            const value = temperatures[i];
+
+            let lastStackIndex = stack[stack.length - 1];
+
+            while (stack.length > 0 && temperatures[lastStackIndex] < value) {
+                result[lastStackIndex] = i - lastStackIndex;
+                stack.pop();
+                lastStackIndex = stack[stack.length - 1];
+            }
+
+            stack.push(i);
+        }
+
+        return result;
+    }
+}
