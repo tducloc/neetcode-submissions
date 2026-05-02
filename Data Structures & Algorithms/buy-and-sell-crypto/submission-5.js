1dcc6f4ef4f15e69d4adc9ac4e08a8f20a1a8e59
Trần Đức Lộc <ducloc0009@gmail.com>
2026-05-02 20:16:22 +0700

    Add: buy-and-sell-crypto - submission-5

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-5.js
@@ -0,0 +1,17 @@
+class Solution {
+    /**
+     * @param {number[]} prices
+     * @return {number}
+     */
+    maxProfit(prices) {
+        let minPrice = Infinity;
+        let maxProfit = 0;
+
+        for (let price of prices) {
+            minPrice = Math.min(minPrice, price);
+            maxProfit = Math.max(maxProfit, price - minPrice);
+        }
+
+        return maxProfit;
+    }
+}
