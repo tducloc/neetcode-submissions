dfa53edac3d27c5e275ad4fd10e2987fc90e8a2e
Trần Đức Lộc <ducloc0009@gmail.com>
2026-05-21 15:06:49 +0700

    Add: reorder-linked-list - submission-0

---
+++ Data Structures & Algorithms/reorder-linked-list/submission-0.js
@@ -0,0 +1,50 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     constructor(val = 0, next = null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {ListNode} head
+     * @return {void}
+     */
+    reorderList(head) {
+        // Quy tắc
+        // Chèn node ở vị trí n-1-index vào sau index
+        // Idea
+        // Loop qua từng node. Tại vị trí i
+        // Backup vị trí next của trỏ 1 => Nếu vị trí next của trỏ 1 là null thì break
+        // Loop tiếp để tìm vị trí cuối
+        // Chèn node ở vị trí cuối vào sau node hiện tại
+        // Move trỏ 1 đến node next ban đầu
+
+        let node = head;
+        while (node && node.next) {
+            const next = node.next;
+
+            let node2 = node.next;
+            while (node2?.next?.next) {
+                node2 = node2.next;
+            }
+
+            if (!node2.next) {
+                break;
+            }
+
+            node.next = node2.next;
+            node2.next = null;
+            if (node.next) {
+                node.next.next = next;
+            }
+
+            node = next;
+        }
+
+        return head;
+    }
+}
